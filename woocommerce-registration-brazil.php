3dbc920e35236afc187ab8d661a422a30276d442
Jonatha Ferreira Pinto <44546147+Jonatha-Ferreira@users.noreply.github.com>
2021-03-11 11:30:14 -0300

    Create woocommerce-registration-brazil.php

---
+++ woocommerce-registration-brazil.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              mailto:ialvsconcelos@gmail.com
+ * @since             1.0.0
+ * @package           WC_Br_Shipping_Only_With_Cep
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WooCommerce Registration Brazil
+ * Description:       Adiciona varios campos na pagina de cadastro do WooCommerce
+ * Version:           1.0
+ * Author:            Jonatha Ferreira
+ * Author URI:        mailto:jonatha.php@gmail.com
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       woocommerce-registration-brazil
+ * Domain Path:       /languages
+ */
